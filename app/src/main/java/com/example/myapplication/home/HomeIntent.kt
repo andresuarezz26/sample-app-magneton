11f45b56452c1cb35b8896a6d366899e4c2cfd37
andresuarezz26 <andresuarezz2693@gmail.com>
2026-07-05 20:08:55 -0500

    [LOCAL-11] Users should be able to find scientific content more easily. The app needs better discovery…

--- app/src/main/java/com/example/myapplication/home/HomeIntent.kt
+++ app/src/main/java/com/example/myapplication/home/HomeIntent.kt
@@ -3,4 +3,6 @@ package com.example.myapplication.home
 sealed interface HomeIntent {
     data object LoadFeed : HomeIntent
     data class LikeVideo(val videoId: String) : HomeIntent
+    data class SelectTopic(val topic: String) : HomeIntent
+    data object ResetFilter : HomeIntent
 }
