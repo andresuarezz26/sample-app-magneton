d640b8e1f796576a290b562c3ba1a742f1863492
andresuarezz26 <andresuarezz2693@gmail.com>
2026-08-03 16:31:41 -0500

    [1] address review comments

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import com.example.myapplication.home.HomeScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.upload.UploadScreen
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
                         Screen.Upload -> UploadScreen(onBack = { currentScreen = Screen.Home })
                     }
                     Text(
-                        text = "Hello World",
+                        text = stringResource(R.string.hello_world),
                         modifier = Modifier
                             .align(Alignment.TopStart)
                             .padding(16.dp)
