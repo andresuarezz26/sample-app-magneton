22b2f76b4ec6f216a4a6aa1bf46c61ba2211c4ec
andresuarezz26 <andresuarezz2693@gmail.com>
2026-08-03 16:27:59 -0500

    [1] Add hello world to main activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.home.HomeScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.myapplication.upload.UploadScreen
@@ -21,9 +27,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 var currentScreen by remember { mutableStateOf(Screen.Home) }
-                when (currentScreen) {
-                    Screen.Home -> HomeScreen(onUploadClick = { currentScreen = Screen.Upload })
-                    Screen.Upload -> UploadScreen(onBack = { currentScreen = Screen.Home })
+                Box {
+                    when (currentScreen) {
+                        Screen.Home -> HomeScreen(onUploadClick = { currentScreen = Screen.Upload })
+                        Screen.Upload -> UploadScreen(onBack = { currentScreen = Screen.Home })
+                    }
+                    Text(
+                        text = "Hello World",
+                        modifier = Modifier
+                            .align(Alignment.TopStart)
+                            .padding(16.dp)
+                    )
                 }
             }
         }
